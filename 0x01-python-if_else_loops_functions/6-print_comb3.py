075b1d090e6c5f3d28a650375eef500069559f3c
GOA29 <goldenbabe2905@gmail.com>
2022-11-29 11:58:14 +0000

    my first commit

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+ in range(100):
+    if int(i / 10) != i % 10 and int(i / 10) < i % 10:
+        print("{}{}".format(int(i / 10), i % 10), end="")
+        if (i != 89):
+            print(", ", end="")
+print("")
